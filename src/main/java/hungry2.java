b48d96fcebba59001d27245691024c446bd3ccdf
aamiraliraza <aamiraliraza@localhost>
2022-11-14 21:54:05 +0100

    Committing branchX

---
+++ src/main/java/hungry2.java
@@ -0,0 +1,14 @@
+
+public class hungry2 {
+	
+	public static void main(String[] args) {
+		
+		String hungry="y";
+		
+		if (hungry=="y") 
+			System.out.println("eat something....");
+		else
+			System.out.println("Do something...");
+	}
+
+}
